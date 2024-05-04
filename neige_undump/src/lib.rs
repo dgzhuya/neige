accb21c57ac371bc37b6932b1b2bdedb3945cbc4
dgzhuya <chenbiuxiu@gmail.com>
2024-05-04 16:30:39 +0800

    🦄 refactor: 修改文件读取功能

--- neige_undump/src/lib.rs
+++ neige_undump/src/lib.rs
@@ -3,7 +3,19 @@ pub mod info;
 
 #[cfg(test)]
 mod tests {
+    use std::io::BufReader;
+
+    use anyhow::{Ok, Result};
+    use neige_infra::read_file;
+
+    use crate::binary::undump;
 
     #[test]
-    fn proto_undump_test() {}
+    fn proto_undump_test() -> Result<()> {
+        let file = read_file("data/test.out")?;
+        let data = BufReader::new(file);
+        #[allow(unused_variables)]
+        let proto = undump(data);
+        Ok(())
+    }
 }
