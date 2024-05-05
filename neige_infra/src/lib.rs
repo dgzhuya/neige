b607398d165ca26c26ecf9e821ca1bb4387fd211
dgzhuya <chenbiuxiu@gmail.com>
2024-05-05 23:17:48 +0800

    🦄 refactor: 模块处理

--- neige_infra/src/lib.rs
+++ neige_infra/src/lib.rs
@@ -1,11 +1,11 @@
 pub mod code;
-// mod constant;
-// mod math;
+mod constant;
+mod math;
 mod proto;
 mod state;
 mod tools;
-// mod value;
+mod value;
 
-// pub use constant::{LUAI_MAXSTACK, LUA_MINSTACK, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS};
+pub use constant::{LUAI_MAXSTACK, LUA_MINSTACK, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS};
 pub use proto::proto::{Constant, LocVar, Prototype, Upvalue};
 pub use tools::space::read_file;
