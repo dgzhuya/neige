9a62980e319e5a395083e6a840048294bead1f06
dgzhuya <chenbiuxiu@gmail.com>
2024-05-06 23:18:49 +0800

    ✨ feat: 添加类型输出

--- neige_infra/src/constant/types.rs
+++ neige_infra/src/constant/types.rs
@@ -12,3 +12,21 @@ pub enum LuaType {
     Thread,
     Integer,
 }
+
+impl LuaType {
+    pub fn type_to_str(&self) -> &str {
+        match self {
+            LuaType::None => "no value",
+            LuaType::Nil => "nil",
+            LuaType::Boolean => "boolean",
+            LuaType::LightUserData => "light user data",
+            LuaType::Number => "number",
+            LuaType::String => "string",
+            LuaType::Table => "table",
+            LuaType::Function => "function",
+            LuaType::UserData => "user data",
+            LuaType::Thread => "thread",
+            LuaType::Integer => "integer",
+        }
+    }
+}
