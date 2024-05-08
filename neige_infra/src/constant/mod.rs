fa4bbf44b2a5451d36354c3bdb3740689391c23d
dgzhuya <chenbiuxiu@gmail.com>
2024-05-08 20:09:23 +0800

    ✨ feat: 修改常量名

--- neige_infra/src/constant/mod.rs
+++ neige_infra/src/constant/mod.rs
@@ -7,5 +7,5 @@ pub use types::LuaType;
 
 pub const LUA_MINSTACK: usize = 20;
 pub const LUAI_MAXSTACK: usize = 1000000;
-pub const LUA_REGISTRYINDEX: isize = -(LUAI_MAXSTACK as isize) - 1000;
+pub const LUA_REGISTRY_INDEX: isize = -(LUAI_MAXSTACK as isize) - 1000;
 pub const LUA_RIDX_GLOBALS: isize = 2;
