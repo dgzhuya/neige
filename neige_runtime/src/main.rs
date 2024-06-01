489f4edf296740e7a9cf2aa3538ea1299f112544
dgzhuya <chenbiuxiu@gmail.com>
2024-06-01 23:04:24 +0800

    ✨ feat: 添加参数读取字节码

--- neige_runtime/src/main.rs
+++ neige_runtime/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io::BufReader;
 
 use neige_infra::read_file;
@@ -5,10 +6,13 @@ use neige_infra::state::CallApi;
 use neige_runtime::state::LuaState;
 
 fn main() {
-    let file = read_file("data/test.out").unwrap();
-    let data = BufReader::new(file);
-    let mut state = LuaState::new();
-    state.aux_lib();
-    state.load(data, "test.out", "bt");
-    state.call(0, 0);
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let file = read_file(&args[1]).unwrap();
+        let data = BufReader::new(file);
+        let mut state = LuaState::new();
+        state.aux_lib();
+        state.load(data, "test.out", "bt");
+        state.call(0, 0);
+    }
 }
