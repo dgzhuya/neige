19961ee9dd5d00d4f9219fdae4bfd63c8c557187
dgzhuya <chenbiuxiu@gmail.com>
2024-06-02 10:05:53 +0800

    🧪 test: 添加测试代码

---
+++ example/type.lua
@@ -0,0 +1,13 @@
+local a, b, c
+c = false       -- boolean
+c = { 1, 2, 3 } -- table
+c = "hello"     -- string
+a = 3.14        -- number
+b = a
+
+print(type(nil))           --> nil
+print(type(true))          --> boolean
+print(type(3.14))          --> number
+print(type("Hello world")) --> string
+print(type({}))            --> table
+print(type(print))         --> function
