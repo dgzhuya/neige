c5169a0a62576c2fa849cf601a50218e163e5442
dgzhuya <chenbiuxiu@gmail.com>
2024-06-02 20:32:03 +0800

    🦄 refactor: allow dead code

--- neige_compiler/src/lexer/mod.rs
+++ neige_compiler/src/lexer/mod.rs
@@ -5,12 +5,13 @@ use std::{
 
 use neige_infra::LuaToken;
 
+#[allow(dead_code)]
 pub struct Lex {
     input: Peekable<Bytes<Box<dyn Read>>>,
     ahead: LuaToken,
     line: usize,
 }
-
+#[allow(dead_code)]
 impl Lex {
     pub fn new<R: Read + 'static>(input: R) -> Self {
         let input: Box<dyn Read> = Box::new(input);
