13a645bd7d69bf3787dcf0b3b877b797ae7d95bb
dgzhuya <chenbiuxiu@gmail.com>
2024-06-08 17:45:54 +0800

    🦄 refactor: 修改内置库调用规则

--- neige_wasm/src/lib.rs
+++ neige_wasm/src/lib.rs
@@ -1,38 +1,20 @@
-use gloo_utils::format::JsValueSerdeExt;
-use neige_infra::{
-    state::{CallApi, LuaApi, PushApi},
-    value::value::LuaValue,
-};
+mod node;
+
+use neige_infra::state::CallApi;
 use neige_runtime::state::LuaState;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+use node::LuaNodeLib;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(module = "/ts/lib.ts")]
 extern "C" {
-    pub fn genFontCode(name: &str, val: &JsValue);
     pub fn logger(info: &str);
 }
-
-fn gen_font_code(ls: &mut dyn LuaApi) -> usize {
-    let info = ls.to_string(-2);
-    let route = if let Some(info) = ls.to_lua_tbl(-1) {
-        if let Ok(val) = JsValue::from_serde(&LuaValue::Table(info)) {
-            val
-        } else {
-            JsValue::null()
-        }
-    } else {
-        JsValue::null()
-    };
-    genFontCode(&info, &route);
-    0
-}
-
 #[wasm_bindgen]
 pub fn run(data: &[u8], file_name: &str) {
     logger("start");
     let mut state = LuaState::new();
     state.aux_lib();
-    state.register("genFontCode", gen_font_code);
+    state.node_lib();
     state.load(data.to_vec(), file_name, "bt");
     state.call(0, 0);
     logger("end")
---
+++ neige_wasm/src/node.rs
@@ -0,0 +1,40 @@
+use gloo_utils::format::JsValueSerdeExt;
+use neige_infra::{
+    state::{GetApi, LuaApi, PushApi, SetApi},
+    value::value::LuaValue,
+};
+use neige_runtime::state::LuaState;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+
+#[wasm_bindgen(module = "/ts/lib.ts")]
+extern "C" {
+    pub fn genFontCode(name: &str, val: &JsValue);
+}
+
+fn gen_font_code(ls: &mut dyn LuaApi) -> usize {
+    let info = ls.to_string(-2);
+    let route = if let Some(info) = ls.to_lua_tbl(-1) {
+        if let Ok(val) = JsValue::from_serde(&LuaValue::Table(info)) {
+            val
+        } else {
+            JsValue::null()
+        }
+    } else {
+        JsValue::null()
+    };
+    genFontCode(&info, &route);
+    0
+}
+
+pub trait LuaNodeLib {
+    fn node_lib(&mut self);
+}
+
+impl LuaNodeLib for LuaState {
+    fn node_lib(&mut self) {
+        self.create_table(0, 1);
+        self.push_rust_fn(gen_font_code);
+        self.set_field(-2, "genFontCode");
+        self.set_global("NodeJs");
+    }
+}
