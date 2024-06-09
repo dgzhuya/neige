bfbb5cc743e6e1b1deb2671e7086b6fa9ae2fc39
dgzhuya <chenbiuxiu@gmail.com>
2024-06-10 00:48:12 +0800

    🎈 perf: 删除wasm

--- neige_wasm/ts/lib.ts
+++
@@ -1,9 +0,0 @@
-export function genFontCode(name: string, val: any) {
-	console.log('name=', name, ',value=', val)
-}
-
-export function logger(info: string, isStart = false) {
-	console.log(
-		`${isStart ? '' : '\n'}${'_'.repeat(10)}${info}${'_'.repeat(10)}\n`
-	)
-}
